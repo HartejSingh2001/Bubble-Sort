0c074c30a606a4ceb664f0101505f6fc8cce8bf4
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-07-23 19:10:51 +0530

    Update Bs.cpp

--- Bs.cpp
+++ Bs.cpp
@@ -1,4 +1,39 @@
  //Bubble sort
+//Apni Kaksha
+int main()
+{
+    cout<<"Bubble sort";
+    int n,i;
+    cout<<"Enter n::";
+    cin>>n;
+    cout<<"Enter the aray elements::";
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int count=1;
+    while(count<n)
+    {
+        for(i=0;i<n-count;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        count++;
+    }
+    cout<<"The sorted elements are::\n";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
+// other method
 class Solution
 {
     public:
